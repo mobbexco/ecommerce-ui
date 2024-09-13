a61639bf6fbb984715ad1152e10817a20a66ff68
mobbexLuis <luis.scarrone@mobbex.com>
2024-09-13 12:10:34 -0300

    improved main file

--- lib/main.ts
+++ lib/main.ts
@@ -1,18 +1,6 @@
 import FinanceWidget from './FinanceWidget/FinanceWidget';
-import FeaturedInstallments from './FinanceWidget/FeaturedInstallments';
-import FeaturedInstallmentDetails from './FinanceWidget/FeaturedInstallmentDetails';
-import SelectCard from './FinanceWidget/SelectCard';
-import SelectInstallment from './FinanceWidget/SelectInstallment';
-import SelectedInstallmentDetails from './FinanceWidget/SelectedInstallmentDetails';
-import Button from './FinanceWidget/Button';
 import './index.css';
 
 export {
-  FinanceWidget,
-  FeaturedInstallments,
-  FeaturedInstallmentDetails,
-  SelectCard,
-  SelectInstallment,
-  SelectedInstallmentDetails,
-  Button,
+  FinanceWidget
 };
