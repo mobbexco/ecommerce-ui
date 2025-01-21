e34c8cca26695c54783785b63cd2808dc6135fa8
mobbexLuis <luis.scarrone@mobbex.com>
2025-01-21 13:40:37 -0300

    added sampleFunction for test

---
+++ lib/Pos/sampleFunction.ts
@@ -0,0 +1,21 @@
+export function getPosSampleData() {
+  return {
+    posList: [
+      {
+        uid: 'awkx9wkq0wxk90k2x',
+        name: 'POS de Prueba',
+        reference: '2934923874982',
+        description: 'Punto de venta de prueba',
+      },
+      {
+        uid: 'xm2k9j2lk3j4l5k6j7',
+        name: 'Café Central',
+        reference: '0987654321098',
+        description: 'Punto de venta para la cafetería Café Central',
+      },
+    ],
+    opUrl: 'http://your-url/',
+    isSellerView: false,
+    handleResetTransaction: (a: any) => {},
+  };
+}
