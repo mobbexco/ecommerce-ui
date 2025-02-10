9a4af0b005a47bde9a0cba43cb4259a633e8ba6f
jlmobbex <javier.lopez@mobbex.com>
2025-02-10 12:06:04 -0300

    Add standalone bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from "vite";
-import { extname, relative, resolve } from "path";
-import { fileURLToPath } from "node:url";
-import { glob } from "glob";
-import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
-import tailwindcss from "tailwindcss";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import tailwindcss from 'tailwindcss';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
-  plugins: [react(), dts({ include: ["lib"] }), cssInjectedByJsPlugin()],
+  plugins: [react(), dts({ include: ['lib'] }), cssInjectedByJsPlugin()],
   css: {
     postcss: {
       plugins: [tailwindcss],
@@ -16,34 +14,8 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: ["react", "react/jsx-runtime", "tailwindcss"],
-      input: Object.fromEntries(
-        glob
-          .sync("lib/**/*.{ts,tsx}", {
-            ignore: ["lib/**/*.d.ts"],
-          })
-          .map((file) => [
-            // The name of the entry point
-            // lib/nested/foo.ts becomes nested/foo
-            relative("lib", file.slice(0, file.length - extname(file).length)),
-            // The absolute path to the entry file
-            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-            fileURLToPath(new URL(file, import.meta.url)),
-          ])
-      ),
-      output: {
-        assetFileNames: "assets/[name][extname]",
-        entryFileNames: "[name].js",
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          tailwindcss: "tailwindcss",
-        },
-      },
+      name: '@mobbex/ecommerce-ui',
+      entry: resolve(__dirname, 'lib/main.ts'),
     },
   },
 });
