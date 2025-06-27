5bd8b599f937c77926c6fdde349cc5f9ac1ca721
mobbexLuis <luis.scarrone@mobbex.com>
2025-06-27 12:40:22 -0300

    -updated vite config to output minified usinf IIFE -improved output dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import react from '@vitejs/plugin-react';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin()],
+  plugins: [react()],
   build: {
-    lib: {
-      name: 'EcoUI',
-      fileName: 'ecommerce-ui',
-      entry: resolve(__dirname, 'lib/main.ts'),
+    rollupOptions: {
+      input: {
+        "finance-widget": "./lib/main.tsx",
+      },
+      output: {
+        format: "iife",
+        entryFileNames: "[name].min.js",
+      },
     },
   },
-  define: { 'process.env.NODE_ENV': '"production"' },
 });
