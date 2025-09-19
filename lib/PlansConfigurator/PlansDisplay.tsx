e7cbe5981cbbfd36e3409eaad51077808ef51b3f
mobbexLuis <luis.scarrone@mobbex.com>
2025-09-19 13:43:36 -0300

    added logic to render checkboxes for each plan isolated from other sources plans

--- lib/PlansConfigurator/PlansDisplay.tsx
+++ lib/PlansConfigurator/PlansDisplay.tsx
@@ -1,19 +1,90 @@
-export default function PlansDisplay() {
-  //logica para renderizar checkbox de planes
-  //logica para renderizar estrellas
+import { useState } from "react";
+import { PaymentSource, Installment } from "../FinanceWidget/Interfaces";
+import { IPlansDisplay } from "./interface";
+import PlansSearcher from "./PlansSearcher";
+
+export default function PlansDisplay({
+  selectedSource,
+  sources,
+  manual,
+}: IPlansDisplay) {
+  // Gets installments for selected source
+  const installments: string[] = sources
+    .filter((item: PaymentSource) => item.source.name === selectedSource)
+    .flatMap((item: PaymentSource) =>
+      item.installments.enabled
+        ? item.installments.list?.map(
+            (installment: Installment) => installment.name
+          ) ?? []
+        : [item.view.subgroup_title]
+    )
+    .filter((i): i is string => Boolean(i));
+
+  // Manages checked plans isolated by source
+  const [checkedPlans, setCheckedPlans] = useState<Record<string, string[]>>(
+    {}
+  );
+  const toggleCheckbox = (source: string, value: string) => {
+    setCheckedPlans((prev) => {
+      const prevForSource = prev[source] || [];
+      return {
+        ...prev,
+        [source]: prevForSource.includes(value)
+          ? prevForSource.filter((v) => v !== value)
+          : [...prevForSource, value],
+      };
+    });
+  };
+
+  const activateAll = (source: string) => {
+    setCheckedPlans((prev) => {
+      const allSelected = (prev[source]?.length || 0) === installments?.length;
+      return {
+        ...prev,
+        [source]: allSelected ? [] : installments,
+      };
+    });
+  };
+
+  const sourceCheckedPlans = checkedPlans[selectedSource] || [];
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const filteredInstallments = installments.filter((i) =>
+    i.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+
   return (
-    <>
-      <span className="mobbex-pc-config-checkbox-title">Selecciona uno de los planes</span>
-      <div className="mobbex-pc-config-checkbox-container">
-        <label className="mobbex-pc-checkbox-label">
-          <input className="mobbex-pc-config-checkbox"type="checkbox" id="box1" value="box1"/>
-          Activar todos los planes
-        </label>
-        <label className="mobbex-pc-checkbox-label">
-          <input className="mobbex-pc-config-checkbox"type="checkbox" id="box1" value="box1"/>
-          12 cuotas sin interés
-        </label>
-      </div>
-    </>
+    selectedSource &&
+    installments?.length > 1 && (
+      <>
+        <PlansSearcher installments={installments} onSearch={setSearchQuery}  />
+        <span className="mobbex-pc-config-checkbox-title">
+          Selecciona uno de los planes
+        </span>
+        <div className="mobbex-pc-config-checkbox-container">
+          <label className="mobbex-pc-checkbox-label">
+            <input
+              className="mobbex-pc-config-checkbox"
+              type="checkbox"
+              checked={sourceCheckedPlans.length === installments.length}
+              onChange={() => activateAll(selectedSource)}
+            />
+            Activar todos los planes
+          </label>
+          {filteredInstallments.map((installment, i) => (
+            <label className="mobbex-pc-checkbox-label" key={i}>
+              <input
+                className="mobbex-pc-config-checkbox"
+                type="checkbox"
+                checked={sourceCheckedPlans.includes(installment)}
+                onChange={() => toggleCheckbox(selectedSource, installment)}
+              />
+              {installment}
+            </label>
+          ))}
+        </div>
+      </>
+    )
   );
 }
