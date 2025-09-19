945a77181e8fcf61fe73a8f634913732332cfa9f
mobbexLuis <luis.scarrone@mobbex.com>
2025-09-19 13:58:40 -0300

    added search handler

--- lib/PlansConfigurator/PlansSearcher.tsx
+++ lib/PlansConfigurator/PlansSearcher.tsx
@@ -1,5 +1,11 @@
-export default function PlansSearcher(installments: string[] | null) {
-  //logica para renderizar sources
+import { IPlansSearcher } from './interface'
+
+export default function PlansSearcher({ onSearch}: IPlansSearcher) {
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onSearch?.(e.target.value);
+  };
+
   return (
     <div className="mobbex-pc-config-search-input">
       <span className="mobbex-pc-config-input-title">Seleccionar planes</span>
@@ -8,16 +14,19 @@ export default function PlansSearcher(installments: string[] | null) {
           className="mobbex-pc-config-input"
           name="select-plans"
           id="select-plans"
-          autoComplete="on"
+          autoComplete="off"
           placeholder="Buscar"
+          aria-label="Buscar planes"
+          onChange={handleInputChange}
         />
-        <button type="submit" title="plans-search-button">
+        <button type="button" title="plans-search-button">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
             height="16"
             fill="currentColor"
             viewBox="0 0 24 24"
+            aria-hidden="true"
           >
             <path d="M10 2a8 8 0 105.293 14.293l4.707 4.707 1.414-1.414-4.707-4.707A8 8 0 0010 2zm0 2a6 6 0 110 12 6 6 0 010-12z" />
           </svg>
