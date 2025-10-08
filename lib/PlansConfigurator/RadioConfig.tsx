59bd6f9968e2e348c877f659bfb0f13b41c8e898
mobbexLuis <luis.scarrone@mobbex.com>
2025-10-08 13:15:03 -0300

    removed unused handlers and added updated global states

--- lib/PlansConfigurator/RadioConfig.tsx
+++ lib/PlansConfigurator/RadioConfig.tsx
@@ -1,64 +1,56 @@
-import { useState, useEffect } from "react";
+import { useContext } from "react";
 import { RadioGroup } from "./RadioGroup";
-import { IRadioConfig } from "./interface";
+import { GlobalContext } from "../context";
 
-export default function RadioConfig({
-  onCustomFeatured,
-  showFeaturedPlans,
-}: IRadioConfig) {
-  // TODO: manejar el estado de configuracion manual globalmente
-  const [showConfig, setShowConfig] = useState(
-    showFeaturedPlans === "yes" ? "show" : "not_show"
-  );
-  const [bestConfig, setBestConfig] = useState("best_three");
-
-  useEffect(() => {
-    setShowConfig(showFeaturedPlans === "yes" ? "show" : "not_show");
-  }, [showFeaturedPlans]);
+export default function RadioConfig() {
+  const { state, setState } = useContext(GlobalContext);
 
   const handleCustomFeatured = (selectedConfig: string) => {
-    setBestConfig(selectedConfig);
-    onCustomFeatured(selectedConfig === "manual");
+    setState({manual: selectedConfig === "manual"});
   };
+
+  const handleShowFeaturedPlans = (selectedConfig: string) => {
+    setState({showFeaturedPlans : selectedConfig === "show"})
+  }
 
   return (
     <>
       <RadioGroup
         name="pc-config-radio-show"
-        defaultValue={showFeaturedPlans === "yes" ? "show" : "not_show"}
-        onChange={setShowConfig}
+        defaultValue={state.showFeaturedPlans ? "show" : "not_show"}
+        onChange={handleShowFeaturedPlans}
         options={[
           {
             id: "not_show",
             value: "not_show",
             label: "Ocultar planes destacados",
-            defaultChecked: showFeaturedPlans == "no"
+            defaultChecked: !state.showFeaturedPlans
           },
           {
             id: "show",
             value: "show",
             label: "Mostrar planes destacados",
-            defaultChecked: showFeaturedPlans == "yes"
+            defaultChecked: state.showFeaturedPlans
           },
         ]}
       />
-      {showConfig === "show" && (
+      {state.showFeaturedPlans && (
         <RadioGroup
           name="pc-config-radio-best"
-          defaultValue={bestConfig}
+          defaultValue={state.manual ? "manual" : "best_three"}
           onChange={handleCustomFeatured}
           options={[
             {
               id: "best_three",
               value: "best_three",
               label: "Mostrar los 3 mejores planes",
-              defaultChecked: bestConfig !== "manual"
+              defaultChecked: !state.manual
             },
             {
               id: "manual",
               value: "manual",
               label: "Definir manualmente los planes destacados",
-              defaultChecked: bestConfig === "manual"
+              defaultChecked: state.manual
             },
           ]}
         />
