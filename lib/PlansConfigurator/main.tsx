fca2a3a7e13856e4d60248c80c7d0b01df505f39
mobbexLuis <luis.scarrone@mobbex.com>
2025-12-05 10:49:20 -0300

    updated interfaces reference updated format with prettier added error logs and improved code readability

--- lib/PlansConfigurator/main.tsx
+++ lib/PlansConfigurator/main.tsx
@@ -1,41 +1,44 @@
-import { createRoot } from 'react-dom/client';
-import PlansConfigurator from './PlansConfigurator';
-import { ISources } from './interface';
+import { createRoot } from "react-dom/client";
+import PlansConfigurator from "./PlansConfigurator";
+import { ISources } from "./interfaces";
 
 declare global {
-    interface Window {
-        mobbexManual: boolean
-        mobbexSources: ISources,
-        platformFormName: string,
-        mobbexFeaturedPlans: string[],
-        mobbexSelectedPlans: string[],
-        mobbexAdvancedPlans: string[],
-        mobbexShowFeaturedPlans: boolean,
-    }
+  interface Window {
+    mobbexManual: boolean;
+    mobbexSources: ISources;
+    platformFormName: string;
+    mobbexFeaturedPlans: string[];
+    mobbexSelectedPlans: string[];
+    mobbexAdvancedPlans: string[];
+    mobbexShowFeaturedPlans: boolean;
+  }
 }
 
 const divId = "mbbx-plans-configurator";
 let root: ReturnType<typeof createRoot> | null = null;
 
 function renderPlansConfigurator() {
-    const container = document.getElementById(divId);
-    if (container) {
-        if (root) {
-            root.unmount();
-        }
-        root = createRoot(container);
-        root.render(
-          <PlansConfigurator
-            manual={window.mobbexManual || false}
-            formName={window.platformFormName || ''}
-            sources={window.mobbexSources || undefined}
-            featuredPlans={window.mobbexFeaturedPlans || []}
-            selectedPlans={window.mobbexSelectedPlans || []}
-            advancedPlans={window.mobbexAdvancedPlans || []}
-            showFeaturedPlans={window.mobbexShowFeaturedPlans || false}
-          />
-        );
-    }
+  const container = document.getElementById(divId);
+
+  // Exit on container not found
+  if (!container)
+    return console.error("Mobbex Plans Configurator container not found");
+
+  // If it's already rendered, unmount it first
+  if (root) root.unmount();
+
+  root = createRoot(container);
+  root.render(
+    <PlansConfigurator
+      manual={window.mobbexManual || false}
+      formName={window.platformFormName || ""}
+      sources={window.mobbexSources || undefined}
+      featuredPlans={window.mobbexFeaturedPlans || []}
+      selectedPlans={window.mobbexSelectedPlans || []}
+      advancedPlans={window.mobbexAdvancedPlans || []}
+      showFeaturedPlans={window.mobbexShowFeaturedPlans || false}
+    />
+  );
 }
 
 renderPlansConfigurator();
